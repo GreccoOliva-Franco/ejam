b4537f6952f886a6676fc9f2f07add1d7ec237eb
Grecco, Franco <srgrecco93@gmail.com>
2025-01-28 12:42:54 +0100

    refactor(heroes): pick selected properties rather than ommited ones for future-proof development

--- src/heroes/dtos/create-hero.dto.ts
+++ src/heroes/dtos/create-hero.dto.ts
@@ -1,4 +1,8 @@
-import { OmitType } from '@nestjs/mapped-types';
+import { PickType } from '@nestjs/mapped-types';
 import { HeroDto } from './hero.dto';
 
-export class CreateHeroDto extends OmitType(HeroDto, ['id']) {}
+export class CreateHeroDto extends PickType(HeroDto, [
+  'name',
+  'superpower',
+  'humility',
+]) {}
